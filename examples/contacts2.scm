418ccb6098b35ba86401714fbfc150ca6b6fcda5
mgushee <matt@gushee.net>
2012-08-29 06:56:29 -0600

    Updated .gitignore

--- examples/contacts2.scm
+++ examples/contacts2.scm
@@ -10,4 +10,19 @@
     (bd: (make-date-step 'birthday default-year-rule: 'none default-month-rule: 'none)))
   looping: #t)
 
-(pp (map queue->list (queue->list (get-all-data))))
+(define (print-csv row)
+  (let loop ((fields '(first last phone email last-contact birthday)))
+    (let ((this-field (car fields))
+          (rest (cdr fields)))
+      (display (alist-ref this-field row))
+      (cond
+        ((null? rest) (newline))
+        ((null? (cdr rest)) (loop rest))
+        (else (display ",") (loop rest))))))
+
+(let ((data (get-all-data)))
+  (let loop ((row (queue-remove! data)))
+    (print-csv (queue->list row))
+    (if (queue-empty? data)
+      #f
+      (loop (queue-remove! data)))))
